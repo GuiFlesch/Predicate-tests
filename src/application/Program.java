79763dc7f2b39fe52bd7bb7456d4c33f98147491
Guilherme Flesch <guilherme.flesch92@gmail.com>
2021-04-26 00:06:10 -0300

    Implementação Interface Function

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
 import entities.Product;
-import util.PriceUpdate;
+import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Program {
 
@@ -20,10 +20,10 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double tax = 1.1;
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 
-        list.forEach(p -> p.setPrice(p.getPrice() * tax));
+        names.forEach(System.out::println);
 
-        list.forEach(System.out::println);
+
     }
 }
